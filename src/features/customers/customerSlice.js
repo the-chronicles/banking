aee7c22f06bf3429132f001d9f7939d6d791ff2a
the-chronicles <oluseungideon@gmail.com>
2023-12-15 16:56:40 +0100

    Worked with classic redux

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -30,5 +30,5 @@ export function createCustomer(fullName, nationalID) {
 }
 
 export function updateName(fullName) {
-  return { type: "account/updateName", payload: fullName };
+  return { type: "customer/updateName", payload: fullName };
 }
